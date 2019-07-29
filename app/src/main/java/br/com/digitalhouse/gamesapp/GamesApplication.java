444ea3e5bfb1e9d8802665ef31f5a6595e737dc5
Eduardo Silvestre <eduardo_mizael@outlook.com.br>
2019-07-29 09:40:13 -0300

    Swipe Refresh

---
+++ app/src/main/java/br/com/digitalhouse/gamesapp/GamesApplication.java
@@ -0,0 +1,15 @@
+package br.com.digitalhouse.gamesapp;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+public class GamesApplication extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+}
--- app/src/main/java/br/com/digitalhouse/gamesapp/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/gamesapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import br.com.digitalhouse.gamesapp.adapter.GamesAdapter;
 import br.com.digitalhouse.gamesapp.model.Game;
 import br.com.digitalhouse.gamesapp.viewmodel.GameViewModel;
@@ -14,12 +15,14 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView gameRecyclerView;
     private GamesAdapter gamesAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout_id);
         gameRecyclerView = findViewById(R.id.games_recycler_view_id);
         gamesAdapter = new GamesAdapter();
 
@@ -31,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
         gameViewModel.atualizarGames();
 
         gameViewModel.getGameLiveData()
-                .observe(this, gameList -> gamesAdapter.atualizarGames(gameList));
+                .observe(this, gameList -> {
+                    gamesAdapter.atualizarGames(gameList);
+                    swipeRefreshLayout.setRefreshing(false);
+                });
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                gameViewModel.atualizarGames();
+            }
+        });
     }
 }
